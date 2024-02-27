0a44009bb81813dc26d63ec7abd22e1d9d2f945f
Alunos Laboratorio <aluno.laboratorio@faesa.br>
2024-02-27 19:23:14 -0300

    (ProjectC): (First Commit)

---
+++ main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	
+	int idade; /* inteiro reserva 4 bytes 32 bits 1 para o sinal*/
+	idade = 56;
+	
+	
+	
+	return 0;
+}
