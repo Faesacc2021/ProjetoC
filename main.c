f4826486b25c90bfa0359624726e9d0b0d94244d
Alunos Laboratorio <aluno.laboratorio@faesa.br>
2024-02-27 20:00:49 -0300

    (ProjectC): (First Commitsecond)

--- main.c
+++ main.c
@@ -7,9 +7,19 @@ int main(int argc, char *argv[]) {
 	
 	
 	int idade; /* inteiro reserva 4 bytes 32 bits 1 para o sinal*/
+	int vetorA[3]; /* tamanho na memoria 4 X 3 */
 	idade = 56;
 	
+	struct student {
+		int matricula;
+		float mensalidade;
+	};
 	
+	struct student studentRecords[30];
+	studentRecords[0].matricula = 21210;
+	studentRecords[0].mensalidade = 1250;
+	
+	printf(studentRecords[0].matricula);
 	
 	return 0;
 }
